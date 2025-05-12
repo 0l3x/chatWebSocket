cd35771e691d35e11f69fa60b375ee00384c8e8e
0l3x <olexanderg3@gmail.com>
2025-05-12 16:32:58 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const nunjucks = require('nunjucks');
+
+let app = express();
+let server = http.createServer(app);
+let io = socketio(server);
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+app.set('view engine', 'njk');
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+server.listen(10000);
+
+io.on('connection', (socket) => {
+    socket.emit('conectado');
+    socket.on('enviar', (datos) => {
+        io.emit('difundir', datos);
+    });
+});
